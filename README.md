ca969b9e8026d3ba1c3e2dff6b9e079fff8ff0e6
khaled swidan <khaledqutb4@gmail.com>
2023-06-30 19:12:43 +0300

    modify README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Mr-Mostafa-Fayes
+Site is live at https://khaledswidan.github.io/mr-mostafa-fayes/
+
+# Screenshot
+Here we have project screenshot :
+
+![screenshot](readme.png)
---
+++ assets/js/main.js
@@ -0,0 +1,127 @@
+(() => {
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener("scroll", listener);
+  };
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select(".back-to-top");
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add("active");
+      } else {
+        backtotop.classList.remove("active");
+      }
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
+
+  /**
+   * Preloader
+   */
+  let preloader = select("#preloader");
+  if (preloader) {
+    window.addEventListener("load", () => {
+      preloader.remove();
+    });
+  }
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener("load", () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false,
+    });
+  });
+
+  //courses isotope and filter;
+  window.addEventListener("load", () => {
+    let coursesContainer = select(".courses-container");
+    if (coursesContainer) {
+      let coursesIsotope = new Isotope(coursesContainer, {
+        itemSelector: ".course-item",
+      });
+
+      let coursesFilters = select("#courses-filters li", true);
+
+      on(
+        "click",
+        "#courses-filters li",
+        function (e) {
+          e.preventDefault();
+          coursesFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          this.classList.add("filter-active");
+
+          coursesIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+          coursesIsotope.on("arrangeComplete", function () {
+            AOS.refresh();
+          });
+        },
+        true
+      );
+    }
+  });
+  /*------------------------------------------*/
+
+  // Navbar links active state on scroll;
+  let navbarlinks = select("#navbar .scrollto", true);
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200;
+    navbarlinks.forEach((navbarlink) => {
+      if (!navbarlink.hash) return;
+      let section = select(navbarlink.hash);
+      if (!section) return;
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
+        navbarlink.classList.add("active");
+      } else {
+        navbarlink.classList.remove("active");
+      }
+    });
+  };
+  window.addEventListener("load", navbarlinksActive);
+  onscroll(document, navbarlinksActive);
+  /*------------------------------------------*/
+})();
